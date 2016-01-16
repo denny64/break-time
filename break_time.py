65bfebb9c3fcb6f7a1e57c6582e10f8e5a5895b7
Denny Vuong <vuongdenny@gmail.com>
2016-01-16 16:42:47 +1100

    init

---
+++ break_time.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+TOTAL_BREAKS = 3
+BREAK_COUNT = 0
+
+print("This program started on" + time.ctime())
+while (BREAK_COUNT < TOTAL_BREAKS):
+    time.sleep(2*60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=j5-yKhDd64s")
+    BREAK_COUNT = BREAK_COUNT + 1
